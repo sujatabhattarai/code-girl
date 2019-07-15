3a8d821f55c368e0548c624b83d9850c25fe0b15
Sujata <bhattaraisuzata@gmail.com>
2019-07-15 18:33:59 -0500

    Added edit and update to events and provide admin authentication for it

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,6 +26,20 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      flash[:success] = "Event was successfully updated"
+      redirect_to event_path(@event)
+    else
+      render 'edit'
+    end
+  end
+
 
   def destroy
     @event= Event.find(params[:id])
